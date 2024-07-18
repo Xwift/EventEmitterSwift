58417ec23ee8e069b3a10ec4d4cc234f38532005
nodeful <romankisilo@gmail.com>
2024-07-18 15:25:39 +0300

    initial implementation, not tested

--- Sources/EventEmitter.swift
+++ Sources/EventEmitter.swift
@@ -1,1 +1,69 @@
+import Foundation
 
+public class EventEmitter<T>: Equatable {
+  package let id = UUID()
+  private let queue: DispatchQueue
+
+  private(set) public var listeners: [EventListener<T>] = []
+
+  public init() {
+    self.queue = DispatchQueue(label: "EventEmitter.\(self.id.uuidString).queue")
+  }
+
+  public func emit(_ value: T) {
+    self.queue.async { [weak self] in
+      guard let self = self else { return }
+      var listenersToRemove: [EventListener<T>] = []
+      for listener in self.listeners.filter({ $0.listening }) {
+        listener.handler(value)
+        if listener.once {
+          listenersToRemove.append(listener)
+        }
+      }
+      self.listeners = self.listeners.filter { !listenersToRemove.contains($0) }
+    }
+  }
+
+  @discardableResult
+  public func on(
+    _ handler: @escaping (T) -> Void,
+    onQueue queue: DispatchQueue? = nil
+  ) -> EventListener<T> {
+    let listener = EventListener(
+      handler: handler,
+      queue: queue
+    )
+
+    self.queue.async { [weak self] in
+      self?.listeners.append(listener)
+    }
+
+    return listener
+  }
+
+  @discardableResult
+  public func once(
+    _ handler: @escaping (T) -> Void,
+    onQueue queue: DispatchQueue? = nil
+  ) -> EventListener<T> {
+    let listener = EventListener(
+      once: true,
+      handler: handler,
+      queue: queue
+    )
+    self.queue.async { [weak self] in
+      self?.listeners.append(listener)
+    }
+    return listener
+  }
+
+  public func off(_ listener: EventListener<T>) {
+    queue.async { [weak self] in
+      self?.listeners = self?.listeners.filter { $0 != listener } ?? []
+    }
+  }
+
+  public static func == (lhs: EventEmitter, rhs: EventEmitter) -> Bool {
+    return lhs.id == rhs.id
+  }
+}
--- Sources/EventListener.swift
+++ Sources/EventListener.swift
@@ -1,19 +1,33 @@
 import Foundation
 
 public class EventListener<T>: Equatable {
-  public let id = UUID()
-  public var handler: (T) -> Void
+  package let id = UUID()
+  public let handler: (T) -> Void
   public var listening: Bool = true
+  package let once: Bool
 
-  package init (handler: @escaping (T) -> Void) {
-    self.handler = handler
+  package init(
+    once: Bool = false,
+    handler: @escaping (T) -> Void,
+    queue: DispatchQueue? = nil
+  ) {
+    self.once = once
+    if let queue = queue {
+      self.handler = { [weak queue] value in
+        queue?.async { handler(value) }
+      }
+    } else {
+      self.handler = { value in
+        handler(value)
+      }
+    }
   }
 
-  public func pause () {
+  public func pause() {
     listening = false
   }
 
-  public func resume () {
+  public func resume() {
     listening = true
   }
 
